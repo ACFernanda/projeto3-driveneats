c83395b03954c0e7d34439c4d01f5f89d8dbbae0
acfernanda <acfernanda98@gmail.com>
2022-01-29 16:59:35 -0300

    enviar mensagem com preco total

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let prato = 0;
 let bebida = 0;
 let sobremesa = 0;
+let valorPrato = 0;
+let valorBebida = 0;
+let valorSobremesa = 0;
 let total = 0;
-
-/*let link =
-  "https://wa.me/5521983480092?" +
-  encodeURIComponent(
-    "Olá, gostaria de fazer o pedido:\n- Prato: 'prato'\n- Bebida: 'bebida'\n- Sobremesa: 'sobremesa'\nTotal: R$ 27.70"
-  );*/
 
 function alterarBotao() {
   if (prato !== 0 && bebida !== 0 && sobremesa !== 0) {
@@ -17,33 +14,36 @@ function alterarBotao() {
   }
 }
 
-function selecionarPrato(botao, nomeExibicao) {
+function selecionarPrato(botao, nomeExibicao, valorPrato) {
   const selecionado = document.querySelector(".pratos .selecionado");
   if (selecionado !== null) {
     selecionado.classList.remove("selecionado");
   }
   prato = nomeExibicao;
+  precoPrato = valorPrato;
   botao.classList.add("selecionado");
   alterarBotao();
 }
 
-function selecionarBebida(botao, nomeExibicao) {
+function selecionarBebida(botao, nomeExibicao, valorBebida) {
   const selecionado = document.querySelector(".bebidas .selecionado");
   if (selecionado !== null) {
     selecionado.classList.remove("selecionado");
   }
   bebida = nomeExibicao;
+  precoBebida = valorBebida;
   botao.classList.add("selecionado");
   alterarBotao();
 }
 
-function selecionarSobremesa(botao, nomeExibicao) {
+function selecionarSobremesa(botao, nomeExibicao, valorSobremesa) {
   const selecionado = document.querySelector(".sobremesas .selecionado");
   if (selecionado !== null) {
     selecionado.classList.remove("selecionado");
     selecionado.classList.remove("icone");
   }
   sobremesa = nomeExibicao;
+  precoSobremesa = valorSobremesa;
   botao.classList.add("selecionado");
   alterarBotao();
 }
@@ -52,6 +52,7 @@ function confirmarPedido() {
   const nome = prompt("Qual é seu nome?");
   const endereço = prompt("Qual é seu endereço?");
   let mensagem;
+  total = precoPrato + precoBebida + precoSobremesa;
   mensagem =
     "Olá, gostaria de fazer o pedido:\n- Prato: " +
     prato +
@@ -59,8 +60,8 @@ function confirmarPedido() {
     bebida +
     "\n- Sobremesa: " +
     sobremesa +
-    "\nTotal: " +
-    total;
+    "\nTotal: R$ " +
+    total.toFixed(2);
 
   let pedido = encodeURIComponent(mensagem);
 
