cbca9063bfe0fada03e8a49fe521981c9e56dee5
acfernanda <acfernanda98@gmail.com>
2022-01-29 16:32:04 -0300

    implementacao mensagem com formatacao

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let prato = 0;
 let bebida = 0;
 let sobremesa = 0;
+let total = 0;
+
+/*let link =
+  "https://wa.me/5521983480092?" +
+  encodeURIComponent(
+    "Olá, gostaria de fazer o pedido:\n- Prato: 'prato'\n- Bebida: 'bebida'\n- Sobremesa: 'sobremesa'\nTotal: R$ 27.70"
+  );*/
 
 function alterarBotao() {
   if (prato !== 0 && bebida !== 0 && sobremesa !== 0) {
     const fecharPedido = document.querySelector(".finalizar");
     fecharPedido.classList.add("finalizar-verde");
-    fecharPedido.innerHTML =
-      "<a href='https://wa.me/5521983480092'>Fechar pedido</a>";
+    fecharPedido.innerHTML = "Fechar pedido";
   }
 }
 
@@ -16,11 +22,8 @@ function selecionarPrato(botao, nomeExibicao) {
   if (selecionado !== null) {
     selecionado.classList.remove("selecionado");
   }
-
   prato = nomeExibicao;
-
   botao.classList.add("selecionado");
-
   alterarBotao();
 }
 
@@ -29,11 +32,8 @@ function selecionarBebida(botao, nomeExibicao) {
   if (selecionado !== null) {
     selecionado.classList.remove("selecionado");
   }
-
   bebida = nomeExibicao;
-
   botao.classList.add("selecionado");
-
   alterarBotao();
 }
 
@@ -43,15 +43,26 @@ function selecionarSobremesa(botao, nomeExibicao) {
     selecionado.classList.remove("selecionado");
     selecionado.classList.remove("icone");
   }
-
   sobremesa = nomeExibicao;
-
   botao.classList.add("selecionado");
-
   alterarBotao();
 }
 
 function confirmarPedido() {
-  let nome = prompt("Qual é seu nome?");
-  let endereço = prompt("Qual é seu endereço?");
+  const nome = prompt("Qual é seu nome?");
+  const endereço = prompt("Qual é seu endereço?");
+  let mensagem;
+  mensagem =
+    "Olá, gostaria de fazer o pedido:\n- Prato: " +
+    prato +
+    "\n- Bebida: " +
+    bebida +
+    "\n- Sobremesa: " +
+    sobremesa +
+    "\nTotal: " +
+    total;
+
+  let pedido = encodeURIComponent(mensagem);
+
+  window.open("https://wa.me/5521983480092?text=" + pedido);
 }
