6a1c4104a84bd3fbb2a174d080313b0cd44d4277
acfernanda <acfernanda98@gmail.com>
2022-01-30 14:27:43 -0300

    versao final

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let prato = 0;
 let bebida = 0;
 let sobremesa = 0;
-let valorPrato = 0;
-let valorBebida = 0;
-let valorSobremesa = 0;
+let precoPrato = 0;
+let precoBebida = 0;
+let precoSobremesa = 0;
 let total = 0;
 
 function alterarBotao() {
@@ -49,7 +49,34 @@ function selecionarSobremesa(botao, nomeExibicao, valorSobremesa) {
   alterarBotao();
 }
 
-function confirmarPedido() {
+function fecharPedido() {
+  const confirmar = document.querySelector(".confirmacao");
+  confirmar.classList.remove("esconder");
+
+  const nomePrato = document.querySelector(".nome-prato");
+  const nomeBebida = document.querySelector(".nome-bebida");
+  const nomeSobremesa = document.querySelector(".nome-sobremesa");
+  const valorPrato = document.querySelector(".preco-prato");
+  const valorBebida = document.querySelector(".preco-bebida");
+  const valorSobremesa = document.querySelector(".preco-sobremesa");
+  const valorTotal = document.querySelector(".preco-total");
+  total = precoPrato + precoBebida + precoSobremesa;
+
+  nomePrato.innerHTML = prato;
+  nomeBebida.innerHTML = bebida;
+  nomeSobremesa.innerHTML = sobremesa;
+  valorPrato.innerHTML = precoPrato.toFixed(2).replace(".", ",");
+  valorBebida.innerHTML = precoBebida.toFixed(2).replace(".", ",");
+  valorSobremesa.innerHTML = precoSobremesa.toFixed(2).replace(".", ",");
+  valorTotal.innerHTML = "R$ " + total.toFixed(2).replace(".", ",");
+}
+
+function cancelar() {
+  const cancelar = document.querySelector(".confirmacao");
+  cancelar.classList.add("esconder");
+}
+
+function finalizarPedido() {
   const nome = prompt("Qual é seu nome?");
   const endereco = prompt("Qual é seu endereço?");
   let mensagem;
